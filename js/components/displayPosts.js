7c708c1cc59777e40c1e8c272615ec6cb60c87c4
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2022-12-13 11:07:16 +0100

    Restructured post elements
    Restructured post elements

--- js/components/displayPosts.js
+++ js/components/displayPosts.js
@@ -7,16 +7,20 @@ function displayPosts(postList) {
         const postDate = truncate (postList[i]["date"], 0, postList[i]["date"].indexOf('T'));
 
         html += `<div class="blog-post">
-                    <a href="ablog.html?postID=${postList[i]["id"]}" title="read" class="ghost-link-blog">
-                        <img src=${imageSrc[0]} class="blog-post-image">
-                    </a>
-                    <a href="ablog.html?postID=${postList[i]["id"]}" title="read" class="ghost-link-blog">
-                        <p class="post-title">${postList[i]["title"]["rendered"]}</p>
-                        <p class="post-date">${postDate}</p>
-                        ${postList[i]["excerpt"]["rendered"]}
-                    </a>
-                    <div class="elementcta-div">
-                        <a href="ablog.html?postID=${postList[i]["id"]}" title="read" class="element-cta landing-cta">Read</a>
+                    <div>
+                        <a href="ablog.html?postID=${postList[i]["id"]}" title="read" class="ghost-link-blog">
+                            <img src=${imageSrc[0]} class="blog-post-image">
+                        </a>
+                    </div>
+                    <div>
+                        <a href="ablog.html?postID=${postList[i]["id"]}" title="read" class="ghost-link-blog">
+                            <p class="post-title">${postList[i]["title"]["rendered"]}</p>
+                            <p class="post-date">${postDate}</p>
+                            ${postList[i]["excerpt"]["rendered"]}
+                        </a>
+                        <div class="elementcta-div">
+                            <a href="ablog.html?postID=${postList[i]["id"]}" title="read" class="element-cta landing-cta">Read</a>
+                        </div>
                     </div>
                 </div>`;
     }
