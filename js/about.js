c847631e22159a8020ef6e3e1eb3ddaa50df9bcb
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2022-12-15 19:28:30 +0100

    Added content to About me page
    Added content to About me page

---
+++ js/about.js
@@ -0,0 +1,21 @@
+const aboutMeContainer = document.querySelector(".about-me");
+
+const aboutURL = "https://www.myblog.casa/wp-json/wp/v2/pages/?about-me";
+
+async function getAboutMe () {
+    aboutMeContainer.innerHTML = `<div class="loading"></div>`;
+
+    try {
+        aResponse = await fetch(aboutURL);
+        aboutMe = await aResponse.json();
+
+        aboutMeContainer.innerHTML = `<h1>${aboutMe[0]["title"]["rendered"]}</h1>
+                                        ${aboutMe[0]["content"]["rendered"]}`;
+    }
+
+    catch (error) {
+        aboutMeContainer.innerHTML = displayMessage("An error ocurred!", "error");
+    }
+}
+
+getAboutMe ();
