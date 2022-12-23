a0de79c7fe1b1224d0910bbf14cf47077d9f17dc
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2022-12-23 14:56:07 +0100

    User test feedback: cont
    Implemented sticky menu
    Added scroll detection

---
+++ js/components/stickyMenu.js
@@ -0,0 +1,35 @@
+const stickyMenuContainer = document.querySelector("header");
+
+stickyMenuContainer.style.display = "block";
+
+let startPosition = 30;
+
+function detectScroll () {
+    const currentPosition = window.scrollY;
+
+    if ((currentPosition - startPosition) < 0) {
+        viewSticky();
+    }
+
+    else if ((currentPosition - startPosition) == 0) {
+    }
+
+    else {
+        hideSticky();
+    }
+
+    startPosition = window.scrollY;
+}
+
+function hideSticky() {
+    stickyMenuContainer.style.display = "none"
+}
+
+function viewSticky() {
+    const stickyStatus = stickyMenuContainer.style.display;
+    if (stickyStatus == "none") {
+        stickyMenuContainer.style.display = "block";
+    }
+}
+
+document.addEventListener("scroll", detectScroll);
