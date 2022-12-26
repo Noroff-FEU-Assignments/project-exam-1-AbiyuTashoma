5245481c13f38ee6f01c93793ad207e6e10ec35c
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2022-12-26 20:53:48 +0100

    improved comment style
    improved comment style

--- js/components/displayComment.js
+++ js/components/displayComment.js
@@ -4,10 +4,13 @@ function displayComment (blogComment) {
     for (let l = 0; l < blogComment.length; l++) {
         const commentDate = truncate (blogComment[l]["date"], 0, blogComment[l]["date"].indexOf('T'));
 
-        html += `<div>
+        const rawContent = blogComment[l]["content"]["rendered"];
+        const content = truncate (rawContent, rawContent.indexOf('>') + 1, rawContent.indexOf('</'));
+
+        html += `<div class="comment-div">
                     <p class="author-name">${blogComment[l]["author_name"]}</p>
                     <p class="comment-date">${commentDate}</p>
-                    ${blogComment[l]["content"]["rendered"]}
+                    <p class="comment-content">${content}</p>
                 </div>`;
     }
 
