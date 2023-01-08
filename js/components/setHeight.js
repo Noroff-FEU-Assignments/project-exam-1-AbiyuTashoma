b228d03d028ea63e999258b79a4f7f4d97455574
AbiyuTashoma <85228220+AbiyuTashoma@users.noreply.github.com>
2023-01-08 16:34:41 +0100

    updated setHeight js
    updated setHeight js

--- js/components/setHeight.js
+++ js/components/setHeight.js
@@ -2,6 +2,5 @@ function setHeight (docElement, deduct = 0) {
     const winHeight = window.screen.availHeight;
     if (winHeight > deduct) {
         docElement.style.minHeight = `${winHeight - deduct}px`;
-        return true;
     }    
 }
